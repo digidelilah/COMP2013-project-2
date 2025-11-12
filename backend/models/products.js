874dbc00aa8b4fab6c76032e1427dd68eafaeddd
BrynnL <brynn.landry2@student.sl.on.ca>
2025-11-12 11:14:13 -0500

    can see the pretty print
    got server to go live and show products from mongo

---
+++ backend/models/products.js
@@ -0,0 +1,31 @@
+//initializing the model scema for products
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create the product model schema
+const productSchema = new Schema({
+    id:{
+        type: String,
+        required: true
+    },
+    productName: {
+        type: String,
+        required: true
+    },
+    brand:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: String,
+        required: true
+    },
+})
+
+//package and export the model
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+//initalizing  server
+const express = require('express');
+const server = express();
+const port = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+const {DB_URI} = process.env;
+const cors = require('cors');//disable default browser security
+const Product = require('./models/products');//import product model schema
+
+//middleware
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+server.use(cors());
+
+//connect to mongodb database
+mongoose.connect(DB_URI).then(() => {
+    server.listen(port,() =>{
+        console.log(`Database is connected\nServer is running on port: ${port}`);
+    });
+}).catch((error) => console.log(error.message));
+
+//Routes
+//root route
+server.get('/', (request,response) => {
+    response.send("server is live");
+});
+
+//get all products
+server.get("/products", async (request, response) => {
+    try{
+        const products = await Product.find();
+        response.send(products);
+    }catch(error){
+        server.status(500).send({message: error.message});
+    }
+});
